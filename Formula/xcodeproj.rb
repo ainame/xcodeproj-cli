cf0bfa6efaaa604fb5dd7ffd06381e5afdb28ca2
ainame <s.namai.09@gmail.com>
2025-07-11 12:14:21 +0100

    Simplify formula

--- Formula/xcodeproj.rb
+++ Formula/xcodeproj.rb
@@ -11,17 +11,4 @@ class Xcodeproj < Formula
   def install
     bin.install "xcodeproj"
   end
-
-  test do
-    # Test help output
-    assert_match "USAGE: xcodeproj", shell_output("#{bin}/xcodeproj --help")
-    
-    # Test creating a project
-    system bin/"xcodeproj", "create", "TestProject", "--organization-name", "Test Org", "--bundle-identifier", "com.test.app"
-    assert_predicate testpath/"TestProject.xcodeproj", :exist?
-    
-    # Test listing targets
-    output = shell_output("#{bin}/xcodeproj list-targets TestProject.xcodeproj")
-    assert_match "TestProject", output
-  end
 end
