7474f1364f6ed4613ef0ad023c28d3fab108a9fd
GitHub Action <action@github.com>
2025-07-21 23:11:35 +0000

    chore: update Formula for 0.2.2

--- Formula/xcodeproj.rb
+++ Formula/xcodeproj.rb
@@ -1,8 +1,8 @@
 class Xcodeproj < Formula
   desc "Command-line tool for manipulating Xcode project files"
   homepage "https://github.com/ainame/xcodeproj-cli"
-  url "https://github.com/ainame/xcodeproj-cli/releases/download/0.2.1/xcodeproj-0.2.1-macos-universal.tar.gz"
-  sha256 "caf8210447ce60ead48495f673cbec8a324ec4b4e9906fdae589c04c98cad9f3"
+  url "https://github.com/ainame/xcodeproj-cli/releases/download/0.2.2/xcodeproj-0.2.2-macos-universal.tar.gz"
+  sha256 "f1f375adf0b10733866d448bf1bed139a9b75e3310e89ccdc5d44fca8e68ba69"
   license "MIT"
   head "https://github.com/ainame/xcodeproj-cli.git", branch: "main"
 
